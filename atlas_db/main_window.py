26ad45f8164fb3c589ce43131ce4abc1c40abd65
Mickael Garcia <mickael.garcia@toonkit-studio.com>
2025-10-14 00:33:23 +0200

    [FIX] Better presentation of Help action

--- atlas_db/main_window.py
+++ atlas_db/main_window.py
@@ -4,9 +4,11 @@ from __future__ import annotations
 
 import sys
 
+from Qt import QtCore as qtc
 from Qt import QtWidgets as qtw
 
 from atlas_db import __version__
+
 
 class AtlasHelp(qtw.QDialog):
     """Help window."""
@@ -15,20 +17,27 @@ class AtlasHelp(qtw.QDialog):
         super().__init__(*args, **kwargs)
         self.setWindowTitle("Help")
 
+        lbl_software = qtw.QLabel("FOST Atlas Asset Manager")
+
         lbl_python_version = qtw.QLabel(
-            f"{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}"
+            f"Python version: {sys.version_info.major}."
+            f"{sys.version_info.minor}.{sys.version_info.micro}"
         )
-        lbl_version = qtw.QLabel(str(__version__))
+        lbl_version = qtw.QLabel(f"Atlas version: {__version__}")
         btn_ok = qtw.QPushButton("Ok")
 
+        lbl_software.setAlignment(qtc.Qt.AlignHCenter)
+        lbl_version.setAlignment(qtc.Qt.AlignHCenter)
+        lbl_python_version.setAlignment(qtc.Qt.AlignHCenter)
 
-        lay_main = qtw.QFormLayout(self)
-        lay_main.addRow("Python version:", lbl_python_version)
-        lay_main.addRow("Atlas version:", lbl_version)
+        lay_main = qtw.QVBoxLayout(self)
+        lay_main.addWidget(lbl_software)
+        lay_main.addWidget(lbl_version)
+        lay_main.addWidget(lbl_python_version)
+        lay_main.addStretch()
         lay_main.addWidget(btn_ok)
 
         btn_ok.clicked.connect(self.accept)
-
 
 
 class AtlasMainWindow(qtw.QMainWindow):
@@ -49,9 +58,6 @@ class AtlasMainWindow(qtw.QMainWindow):
     def _help_menu(self):
         dlg = AtlasHelp()
         dlg.exec()
-
-
-
 
 
 if __name__ == "__main__":
